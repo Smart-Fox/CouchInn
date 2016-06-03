419066c661a6a8808a5ba30282d215aa3af53dba
MatiasGC <matiasgc@gmail.com>
2016-06-03 10:54:51 -0300

    Add files via upload

---
+++ verPerfil/anuncDetalle.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Detalle de anuncio</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel='stylesheet' href='style.css'/>
+	<script language= "javascript" src= "js/validation.js"></script>
+</head>
+<body>
+	<?php
+		$id=$_POST['anunc'];
+		include('anuncioService.php');
+		include('header.php');
+		session_start();
+		if(isset($_SESSION['usuario'])){
+			$service = new cabecera($_SESSION['usuario']);
+			$service->buildHeader();
+			$serv = new aService();
+			$anun = $serv->levantarAnuncio($id);
+			$row = $anun->fetch_assoc();
+			$imagen = $serv->levantarImagen($row['ID']);
+			$row1 = $imagen->fetch_assoc();
+			$link = $row1['enlace'];
+			$res =	$serv->levantarAnuncioCiudad($row['ID_ciudad']);
+			$ciudad = $res->fetch_assoc();
+			$res =	$serv->levantarAnuncioProv($ciudad['ID_provincia']);
+			$prov = $res->fetch_assoc();
+			$res =	$serv->levantarAnuncioAutor($row['ID_usuario']);
+			$user = $res->fetch_assoc();
+			$res =	$serv->levantarAnuncioTipo($row['ID_tipo_hospedaje']);
+			$tipo = $res->fetch_assoc();
+			echo "	<div class='row'>
+						<div class='col-xs-1 col-md-1'>
+						</div>
+						<div class='col-xs-9 col-md-9'>
+							<h1><strong><span>".$row['Titulo']."</span></strong></h1>
+						</div>
+						<div class='col-xs-2 col-md-2'>
+						</div>
+					</div>
+					<div class='row'>
+						<div class='col-xs-1 col-md-1'>
+						</div>
+						<div class='col-xs-5 col-md-5'>		
+							<img src= img/".$link." width='500'>
+						</div>
+						<div class='col-xs-5 col-md-5'>		
+							Tipo de Hospedaje: ".$tipo['Nombre']."
+							<br>
+							Capacidad: ".$row['Capacidad']." personas
+							<br>
+							Descripción: ".$row['Descripcion']."
+							<br>
+							Provincia: ".$prov['Nombre']."
+							<br>
+							Ciudad:	".$ciudad['nombre']."
+							<br>
+							Autor: ".$user['Username']."
+							<br>
+						</div>
+						<div class='col-xs-1 col-md-1'>
+						</div>
+					</div>
+					<br>
+					<div class='row'>
+						<div class='col-xs-4 col-md-4'>
+						</div>
+						<div class='col-xs-4 col-md-4'>
+							<a href='pagPrinc.php'><button class='btn'>Volver</button></a> <a href=\"verPerfil.php?id=".$row['ID_usuario']."\"><button class='btn'>Ver perfil del usuario</button></a>
+
+						</div>
+						<div class='col-xs-4 col-md-4'>
+
+						</div>
+					</div>
+					";
+		}else{
+			header('Location:index.html');
+		}
+	?>
+	
+</body>
+</html>
---
+++ verPerfil/header.php
@@ -0,0 +1,66 @@
+<?php
+	class cabecera {
+		protected $user;
+
+		public function __construct($us){
+			$this->user = $us;
+		}
+
+		public function buildHeader(){
+			switch ($_SESSION['type']){
+				case ("admin"):
+						echo "	<div class=\"header\">
+									<div class='row'>
+										<div id=\"logo\" class='col-xs-4 col-md-4'>
+											<a href=\"pagPrinc.php\"><img src=logo.png width=200></a>
+										</div>
+										<div id=\"vertcentered2\" class='col-xs-4 col-md-4'>
+											<span>Bienvenido ", $_SESSION['usuario'], "</span> 
+
+
+										</div>
+										<div id=\"opcionesuser\" class='col-xs-4 col-md-4'>
+											<a href=\"panelAdmin.php\"><button type=button class='btn2'>Panel de administrador</button></a>
+											<a href=\"cerrarSesion.php\"><button type=button class='btn2'>Cerrar Sesión</button></a>
+										</div>
+									</div>
+								</div>";
+					break;
+				case ("premium"):
+					echo "	<div class=\"header\">
+								<div class='row'>
+									<div id=\"logo\" class='col-xs-4 col-md-4'>
+										<a href=\"pagPrinc.php\"><img src=logo.png width=200></a>
+									</div>
+									<div id=\"vertcentered2\" class='col-xs-4 col-md-4'>
+											<span>Bienvenido ", $_SESSION['usuario'], "</span>
+									</div>
+									<div id=\"opcionesuser\" class='col-xs-4 col-md-4'>
+										<a href=\"cerrarSesion.php\"><button type=\"button\" class=\"btn2\">Cerrar Sesión</button></a>
+									</div>
+								</div>
+							</div>";
+					break;
+				case ("common"):
+					echo "	<div class=\"header\">
+								<div class='row'>
+									<div id=\"logo\" class='col-xs-4 col-md-4'>
+										<a href=\"pagPrinc.php\"><img src=logo.png width=200></a>
+									</div>
+									<div id=\"vertcentered2\" class='col-xs-4 col-md-4'>
+											<span>Bienvenido ", $_SESSION['usuario'], "</span> <a href=\"verPerfil.php?id=".$_SESSION['id']."\"><button class='btn' type = button><span>Ver Perfil</span></button></a>
+
+									</div>
+									<div id=\"opcionesuser\" class='col-xs-4 col-md-4'>
+										<a href=\"comprarPrem.php\"><button type=button class='btn2'>Comprar premium</button></a>
+										<a href=\"cerrarSesion.php\"><button type=\"button\" class=\"btn2\">Cerrar Sesión</button></a>
+									</div>
+								</div>
+							</div>";
+					break;
+			}
+		}
+
+	}
+	
+?>
