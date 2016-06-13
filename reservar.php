fd235ae709deee5e7f117551dc6dc6b23e32f8a0
radbtr <radb89@hotmail.com>
2016-06-12 22:05:24 -0300

    agregado calendario
    todavia no funciona el reservar, pero me voy acercando

--- reservar.php
+++ reservar.php
@@ -5,11 +5,96 @@
 	<title>Solicitar reserva</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link href="theme/jquery-ui.css" rel="stylesheet">
 	<link rel='stylesheet' href='style.css'/>
 	<script src="js/jquery.min.js"></script>
+	<script src="theme/jquery-ui.js"></script>
 	<script type="text/javascript" src= "js/objeto.js"></script>
 	<script type="text/javascript" src="js/bootstrap-filestyle.min.js"> </script>
-	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
+	<script>
+		jQuery(function($){
+			$.datepicker.regional['es'] = {
+				closeText: 'Cerrar',
+				prevText: '&#x3c;Ant',
+				nextText: 'Sig&#x3e;',
+				currentText: 'Hoy',
+				monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio',
+				'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+				monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun',
+				'Jul','Ago','Sep','Oct','Nov','Dic'],
+				dayNames: ['Domingo','Lunes','Martes','Mi&eacute;rcoles','Jueves','Viernes','S&aacute;bado'],
+				dayNamesShort: ['Dom','Lun','Mar','Mi&eacute;','Juv','Vie','S&aacute;b'],
+				dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','S&aacute;'],
+				weekHeader: 'Sm',
+				dateFormat: 'yy/mm/dd',
+				firstDay: 1,
+				isRTL: false,
+				showMonthAfterYear: false,
+				yearSuffix: ''};
+			$.datepicker.setDefaults($.datepicker.regional['es']);
+			var dateToday=new Date();
+			$("#final").datepicker();
+			$("#inicial").datepicker({  
+			  minDate: dateToday,   
+			  onSelect:function(ui, event) { $('#final').datepicker("option","minDate",ui);}  
+			  });
+		});			
+		
+		var rangesS=[];
+		var rangesE=[];
+
+		$(function cargarFechas() {
+			var inicialArray = eval();
+			var finalArray = eval();
+			for(i=0;i<finalArray.length;i++){
+				var from=inicialArray[i].fecha.split("-");
+				var from2=finalArray[i].fecha.split("-");
+				rangesS[i]= new Date(from[0],from[1]-1,from[2]);
+				rangesE[i]= new Date(from2[0],from2[1]-1,from2[2]);
+			}
+		});
+
+		$(function() { 
+			$("#inicial").datepicker({ 
+				numberOfMonths: 2,
+				beforeShowDay: function(date) {
+					var dateToday=new Date();
+					dateToday.setDate(dateToday.getDate()-1);
+					for(var i=0; i<rangesS.length; i++) {
+						if(date<dateToday){
+							return [false, ''];
+						}else{
+							if(date >= rangesS[i] && date <= rangesE[i]){
+								return [true, '']; 
+							}
+						}
+					}
+					return [false, ''];
+				} 
+			}); 
+		});
+
+		$(function() { 
+			$("#final").datepicker({ 
+				numberOfMonths: 2,
+				beforeShowDay: function(date) {
+					var dateToday=new Date();
+					dateToday.setDate(dateToday.getDate()-1);
+					for(var i=0; i<rangesS.length; i++) {
+						if(date<dateToday){
+							return [false, ''];
+						}else{
+							if(date >= rangesS[i] && date <= rangesE[i]){
+								return [true, '']; 
+							}
+						}
+					}
+					return [false, ''];
+				} 
+			}); 
+		});
+	</script>
 </head>
 <body>	
 	<?php
@@ -43,10 +128,12 @@
 						<input type="number" name = 'cantidad' id='cantidad' min="1" placeholder="Ej: 3" required>
 					</div>
 					<div class='row'>
-						<span class='labelform2'>Fecha inicio:</span><br>
+						<span class='labelform2'>Fecha inicio:</span>
+						<input type="text"  class="form-control" placeholder="Fecha Inicial" name="inicial" id="inicial">
 					</div>
 					<div class='row'>
-						<span class='labelform2'>Fecha fin:</span><br>
+						<span class='labelform2'>Fecha fin:</span>
+						<input type="text" class="form-control" placeholder="Fecha Final" name="final" id="final">
 					</div>
 				</div>
 				<div class='col-xs-2 col-md-2'>
