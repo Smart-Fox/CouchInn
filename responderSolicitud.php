19c1ee987b1e21e1b665055536c53d903d31d656
MatiasGC <matiasgc@gmail.com>
2016-06-16 12:46:00 -0300

    act

---
+++ responderSolicitud.php
@@ -0,0 +1,32 @@
+<?php
+	include('anuncioService.php');
+	session_start();
+	$idSolic=$_POST['solic'];
+	$resp=$_POST['resp'];
+	$idUser=$_SESSION['id'];
+	$serv = new aService();
+	$solic = $serv->levantarSolicitud($idSolic);
+	$row=$solic->fetch_assoc();
+	switch($resp){
+		case 'cancelar':
+			if ($row['ID_usuario']==$idUser){
+				$serv->cancelarSolicitudHuesped($idSolic);
+			}else{
+				$serv->cancelarSolicitudAutor($idSolic);
+			}
+		break;
+		
+		case 'aceptar':
+		
+		break;
+		
+		case 'rechazar':
+		
+		break;
+	}
+	if ($row['ID_usuario']==$idUser){
+		header('Location: solicitudesEnv.php');
+	}else{
+		header('Location: solicitudesRec.php');
+	}
+?>
