e00b27f48e3566a9fe118f2b66deeff420bf3b42
MatiasGC <matiasgc@gmail.com>
2016-06-23 20:50:22 -0300

    agora si

--- verPerfil.php
+++ verPerfil.php
@@ -10,9 +10,6 @@
 	<script src="js/jquery.min.js"></script>
 	<script type="text/javascript" src= "js/objeto.js"></script>
 	<script type="text/javascript" src="js/bootstrap-filestyle.min.js"> </script>
-	<script type="text/javascript" src= "js/not.js"></script>
-	<script type="text/javascript" src= "js/verSolicitudes.js"></script>
-	<script type="text/javascript" src= "js/ver.js"></script>
 </head>
 <body>
 	<?php
@@ -60,6 +57,7 @@
 										Tipo Usuario:	<strong><span class='titulo2'>".$tipo."</span></strong>
 									</h3>
 									<a href='editUser.php'><button class='btn22'>Editar</button></a>
+									
 								</div>
 							</div>
 						</div>
@@ -96,30 +94,60 @@
 						</div>";
 			if($anun){
 				while($row = $anun->fetch_assoc()){
-					echo "	<form action='anuncDetalle.php' method='POST' enctype='multipart/form-data'>
-								<div class='row'>
-									<div class='col-xs-2 col-md-2'>
-									</div>
-									<div class='col-xs-8 col-md-8 anuncio'>
-										<input class=hidden name='anunc' value=\"".$row['anuncio_ID']."\">
-											<button type='submit' class='buttonlink'>
-												<div class='row'>
-													<div class='col-xs-3 col-md-3' id='img'>
-														<img src= img/".$row['enlace']." class='imgAnun'>
+					if ($_SESSION['id'] == $id){	//si es el mismo usuario del anuncio se publican todos los anuncios
+						echo "	<form action='anuncDetalle.php' method='POST' enctype='multipart/form-data'>
+									<div class='row'>
+										<div class='col-xs-2 col-md-2'>
+										</div>
+										<div class='col-xs-8 col-md-8 anuncio'>
+											<input class=hidden name='anunc' value=\"".$row['anuncio_ID']."\">
+												<button type='submit' class='buttonlink'>
+													<div class='row'>
+														<div class='col-xs-4 col-md-4'>
+															<img src= img/".$row['enlace']." class=imgAnun align='center'>
+														</div>
+														<div class='col-xs-8 col-md-8'>
+															<h2>
+																<strong><span class='titulo'>".$row['Titulo']."</span></strong>
+															</h2>
+														</div>
 													</div>
-													<div class='col-xs-9 col-md-9'>
-														<h2>
-															<strong><span class='titulo'>".$row['Titulo']."</span></strong>
-														</h2>
+												</button>
+											</input>
+										</div>
+										<div class='col-xs-2 col-md-2'>
+										</div>
+									</div>
+								</form>";
+					}else{
+						if ($row['activo'] == '1'){		// si no es el mismo del anuncio se publican solo las que están activas
+
+							echo "	<form action='anuncDetalle.php' method='POST' enctype='multipart/form-data'>
+									<div class='row'>
+										<div class='col-xs-2 col-md-2'>
+										</div>
+										<div class='col-xs-8 col-md-8 anuncio'>
+											<input class=hidden name='anunc' value=\"".$row['anuncio_ID']."\">
+												<button type='submit' class='buttonlink'>
+													<div class='row'>
+														<div class='col-xs-4 col-md-4'>
+															<img src= img/".$row['enlace']." class=imgAnun align='center'>
+														</div>
+														<div class='col-xs-8 col-md-8'>
+															<h2>
+																<strong><span class='titulo'>".$row['Titulo']."</span></strong>
+															</h2>
+														</div>
 													</div>
-												</div>
-											</button>
-										</input>
+												</button>
+											</input>
+										</div>
+										<div class='col-xs-2 col-md-2'>
+										</div>
 									</div>
-									<div class='col-xs-2 col-md-2'>
-									</div>
-								</div>
-							</form>";
+								</form>";
+						}
+					}
 				}
 			}
 	?>
