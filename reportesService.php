a4ba65b864ceabadff5ecea6ea211a39b1eafd7c
radbtr <radb89@hotmail.com>
2016-07-10 19:53:43 -0300

    consultas reportes

--- reportesService.php
+++ reportesService.php
@@ -2,17 +2,22 @@
 	
 	include_once('dbManager.php');
 	
-	public function reportePremium(){
+	public function reportePremium($inicial, $final){
 		$conec = new dbManager();
 		$conec->conectar();	
-		$consulta = ("SELECT * FROM  WHERE ;");
+		$consulta = ("SELECT *	FROM pago
+								INNER JOIN usuario on usuario.ID_pago=pago.ID
+								WHERE fecha <=$final AND fecha>=$inicial;");
 		return ($conec->ejecutarSQL($consulta));
 	}
 	
-	public function reporteReserva(){
+	public function reporteReserva($inicial, $final){
 		$conec = new dbManager();
 		$conec->conectar();	
-		$consulta = ("SELECT * FROM  WHERE ;");
+		$consulta = ("SELECT *	FROM solicitud_reserva
+								INNER JOIN usuario on solicitud_reserva.ID_usuario=usuario.ID
+								INNER JOIN anuncio on solicitud_reserva.ID_anuncio=anuncio.ID
+								WHERE fecha_solicitud <=$final AND fecha>=$inicial;");
 		return ($conec->ejecutarSQL($consulta));
 	}
 	
