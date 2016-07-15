7ba2f40e3fd35d1e3b7fb0e931a26ca13784d284
radbtr <radb89@hotmail.com>
2016-07-15 07:37:57 -0300

    preparando

--- cuentaOptions.php
+++ cuentaOptions.php
@@ -14,8 +14,6 @@
 								</div>
 							</div>
 							<div class='col-xs-2 col-md-2'>
-							</div>
-							<div class='col-xs-2 col-md-2'>
 								<div class='centered'>
 									<a href='preguntas.php'><button type=button class='btn2'>Mis preguntas</button></a>
 								</div>
@@ -29,7 +27,21 @@
 								</div>
 							</div>
 							<div class='col-xs-2 col-md-2'>
+								<div class='centered'>
+									<form method='POST' action='misreservas.php'>
+										<input class='hidden' name='tipo' value='norm'>
+										<button type=submit class='btn2'>Mis reservas</button>
+									</form>
+								</div>
 							</div>
+							<div class='col-xs-2 col-md-2'>
+								<div class='centered'>
+										<form method='POST' action='miscalificaciones.php'>
+											<input class='hidden' name='tipo' value='norm'>
+											<button type=submit class='btn2'>Mis calificaciones</button>
+										</form>
+									</div>
+								</div>
 							<div class='col-xs-2 col-md-2'>
 								<div class='centered'>
 									<a href='pagPrinc.php'><button type=button class='btn2'>Salir</button></a>
